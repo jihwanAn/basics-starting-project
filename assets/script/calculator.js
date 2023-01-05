84b480b94848345903d8058f5d0a5f9a88e7f8b1
[Github <[Github>
2023-01-05 18:59:46 +0900

    버튼들 함수에 연결

--- assets/script/calculator.js
+++ assets/script/calculator.js
@@ -6,3 +6,24 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
   const calcDescripton = `${resultBeforeCalc} ${operator} ${calcNumber}`;
   outputResult(currentResult, calcDescripton);
 }
+
+function add() {
+  calcResult("ADD");
+}
+
+function subtract() {
+  calcResult("SUBTRACT");
+}
+
+function multiply() {
+  calcResult("MULTIPLY");
+}
+
+function divide() {
+  calcResult("DIVIDE");
+}
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
